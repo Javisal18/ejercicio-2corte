263d89cda15eb029f0e94530a8429d87c2843360
Javisal18 <SALCEDOJAVIER211@GMAIL.COM>
2024-09-10 22:47:47 -0500

    Add files via upload

---
+++ principal/main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package principal;
+
+import formularios.frmpersonas;
+
+/**
+ *
+ * @author el_ja
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       frmpersonas p= new frmpersonas();
+       p.setVisible(true); 
+       
+    }
+    
+}
